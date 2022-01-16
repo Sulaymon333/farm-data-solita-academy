3240fd67d608d367fbd1fd82d67455eb554a40ed
sulaymon333 <tjsulaymon@gmail.com>
2022-01-16 18:39:55 +0200

    implemented basic page components

---
+++ src/pages/FarmInfo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FarmInfo = () => {
+    return <div>farm info</div>;
+};
+
+export default FarmInfo;
---
+++ src/pages/FarmStats.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FarmStats = () => {
+    return <div>farm stats</div>;
+};
+
+export default FarmStats;
