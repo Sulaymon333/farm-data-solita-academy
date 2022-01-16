521bb7bcbd8720f7af25a5c167cc14403d867b55
sulaymon333 <tjsulaymon@gmail.com>
2022-01-16 19:08:12 +0200

    implemented farm stats page

--- src/pages/FarmStats.js
+++ src/pages/FarmStats.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useFetch } from '../hooks/fetch';
+import { useLocation, Link } from 'react-router-dom';
+
+import Loading from '../components/Loading';
+import Table from '../components/FarmDataTable';
 
 const FarmStats = () => {
-    return <div>farm stats</div>;
+    const { pathname: url } = useLocation();
+
+    const { data, loading } = useFetch(url);
+    const farmLocations = ['Metsola', 'Viikki', 'Tampere', 'Eteläesplanadi'];
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    return (
+        <div className='center'>
+            <div className='flex'>
+                <div className='farm-info'>
+                    <h1>Farm Id: {data.measurements[0].farm_id}</h1>
+                    <p>Farm Name: {data.measurements[0].location}</p>
+                    <p>Farm Location: {farmLocations[Number(data.measurements[0].farm_id) - 1]}</p>
+                    <div className='btn-container'>
+                        <Link to='/' className='btn'>
+                            back home
+                        </Link>
+                    </div>
+                </div>
+                <Table data={data.measurements} tableType='measurements' />
+            </div>
+        </div>
+    );
 };
 
 export default FarmStats;
