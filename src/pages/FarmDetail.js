b20bd68b9b208fa9bd1b9ed8bc33bfc406073b6b
sulaymon333 <tjsulaymon@gmail.com>
2022-01-16 19:05:26 +0200

    implemented farm detail page and styles it

--- src/pages/FarmDetail.js
+++ src/pages/FarmDetail.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { useFetch } from '../hooks/fetch';
 
 const FarmDetail = () => {
-    return <div>farm detail</div>;
+    const { pathname: url } = useLocation();
+
+    const { data, loading } = useFetch(url);
+
+    if (loading) {
+        return (
+            <div className='center'>
+                <p className='loading'>Loading...</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className='center'>
+            {data && (
+                <div className='farm-info'>
+                    <h1>Farm Id: {data.farm_id}</h1>
+                    <p>Farm Name: {data.name}</p>
+                    <p>Farm Location: {data.location}</p>
+                    <p>File Name: {data['file-name']}</p>
+                    <p>Date Updated: {data.established}</p>
+                    <div className='btn-container'>
+                        <Link to={`${url}/stats`} className='btn btn-primary'>
+                            Farm statistics
+                        </Link>
+                        <Link to='/' className='btn btn-primary'>
+                            back home
+                        </Link>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default FarmDetail;
